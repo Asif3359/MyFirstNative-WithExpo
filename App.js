616addc6d4ecf38fe3ee419ae104eb416910cff6
Asif359 <asifahammednishst@gmail.com>
2023-10-22 19:05:57 +0600

    using a components using props

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const GreatIng = props =>{
+
+  return(
+    <View>
+      <Text>My name is {props.name}</Text>
+    </View>
+  );
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Native App </Text>
-      <StatusBar style="auto" />
+      <GreatIng name="asif"></GreatIng>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
