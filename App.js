e485afb214dc1f75ff0dd1e2f8d0ce56c572bd94
Asif359 <asifahammednishst@gmail.com>
2023-10-22 19:29:20 +0600

    using useState hook

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const GreatIng = props =>{
-
+  
   return(
     <View>
       <Text>My name is {props.name}</Text>
@@ -11,11 +12,17 @@ const GreatIng = props =>{
 }
 
 export default function App() {
+  const [count , setCount]=useState(0);
+  const handleClik=()=>{
+    setCount(count+1);
+  }
   return (
     <View style={styles.container}>
       <Text>Hello Native App </Text>
       <GreatIng name="asif"></GreatIng>
       {/* <StatusBar style="auto" /> */}
+      <p>Count :{count}</p>
+      <button onClick={()=>handleClik()} >click me</button>
     </View>
   );
 }
